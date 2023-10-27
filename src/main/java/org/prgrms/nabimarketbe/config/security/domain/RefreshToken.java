2b04a6edc62d5e53278291b72d91a161c4c060b6
born-A <ra_ari@naver.com>
2023-10-28 00:32:43 +0900

    feat : kakao 인증

---
+++ src/main/java/org/prgrms/nabimarketbe/config/security/domain/RefreshToken.java
@@ -0,0 +1,36 @@
+package org.prgrms.nabimarketbe.config.security.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.prgrms.nabimarketbe.user.BaseTimeEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "refresh_token")
+@Getter
+@NoArgsConstructor
+public class RefreshToken extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column(nullable = false)
+    private Long key;
+
+    @Column(nullable = false)
+    private String token;
+
+    public RefreshToken updateToken(String token) {
+        this.token = token;
+        return this;
+    }
+
+    @Builder
+    public RefreshToken(Long key, String token) {
+        this.key = key;
+        this.token = token;
+    }
+}
