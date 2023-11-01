44434ef39ff7318739fd2c627a0968007b905698
born-A <ra_ari@naver.com>
2023-11-02 00:46:26 +0900

    NABI-102-refactor : 서비스로 uri 생성 책임 이동, 하나의 메서드를 통해 결과 profile 반환

--- src/main/java/org/prgrms/nabimarketbe/oauth2/kakao/api/OAuth2Controller.java
+++ src/main/java/org/prgrms/nabimarketbe/oauth2/kakao/api/OAuth2Controller.java
@@ -9,13 +9,11 @@ import org.prgrms.nabimarketbe.global.util.ResponseFactory;
 import org.prgrms.nabimarketbe.oauth2.kakao.dto.KakaoProfile;
 import org.prgrms.nabimarketbe.oauth2.kakao.service.OAuth2Service;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.*;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -35,39 +33,21 @@ public class OAuth2Controller {
 
     private final ResponseFactory responseFactory;
 
-    @Value("${spring.url.base}")
-    private String baseUrl;
-
-    @Value("${social.kakao.client-id}")
-    private String kakaoClientId;
-
-    @Value("${social.kakao.redirect}")
-    private String kakaoRedirectUri;
-
     @GetMapping("/login")
     public void socialLogin(HttpServletResponse response) throws IOException {
-        StringBuilder loginUri = new StringBuilder()
-                .append(env.getProperty("social.kakao.url.login"))
-                .append("?response_type=code")
-                .append("&client_id=").append(kakaoClientId)
-                .append("&redirect_uri=").append(baseUrl).append(kakaoRedirectUri);
-
+        StringBuilder loginUri = OAuth2Service.createUri();
         response.sendRedirect(String.valueOf(loginUri));
     }
 
     @GetMapping(value = "/redirect")
     public CommonResult redirectKakao(@RequestParam String code) {
-        //받은 token info 에서 acccess token 추출
-        String accessToken = OAuth2Service.getKakaoTokenInfo(code).getAccess_token();
+        KakaoProfile profile = OAuth2Service.getResultProfile(code);
+        if (profile == null) throw new RuntimeException("카카오에 해당 회원이 없습니다.");
 
-        log.info(accessToken);
-
-        KakaoProfile kakaoProfile = OAuth2Service.getKakaoProfile(accessToken);
-        if (kakaoProfile == null) throw new RuntimeException("카카오에 해당 회원이 없습니다.");
-
-        return signService.signInBySocial(kakaoProfile);
+        return signService.signInBySocial(profile);
     }
 
+    //TODO : 사용자가 accessToken 넘기는건 아닌거 같음
     @GetMapping(value = "/unlink")
     public CommonResult unlinkKakao(@RequestParam String accessToken) {
         String unlinkUri = env.getProperty("social.kakao.url.unlink");
