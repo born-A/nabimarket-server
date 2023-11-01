dd849f3e7ac5e8c5eec5e07d7f9bc816a2aa101d
BeommoKoo-dev <95630007+BeommoKoo-dev@users.noreply.github.com>
2023-11-01 17:55:37 +0900

    NABI-101-refactor : 각 서비스의 책임에 맞게 리팩토링

--- src/main/java/org/prgrms/nabimarketbe/domain/user/service/SignService.java
+++ src/main/java/org/prgrms/nabimarketbe/domain/user/service/SignService.java
@@ -1,32 +1,34 @@
 package org.prgrms.nabimarketbe.domain.user.service;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.prgrms.nabimarketbe.global.security.jwt.dto.TokenRequestDto;
-import org.prgrms.nabimarketbe.oauth2.kakao.dto.KakaoProfile;
-import org.prgrms.nabimarketbe.oauth2.kakao.service.OAuth2Service;
+import java.util.Optional;
+
 import org.prgrms.nabimarketbe.domain.user.dto.sign.UserSignupRequestDto;
-import org.prgrms.nabimarketbe.global.security.jwt.provider.JwtProvider;
-import org.prgrms.nabimarketbe.global.security.entity.RefreshToken;
-import org.prgrms.nabimarketbe.oauth2.kakao.repository.RefreshTokenJpaRepo;
-import org.prgrms.nabimarketbe.global.security.jwt.dto.TokenDto;
 import org.prgrms.nabimarketbe.domain.user.entity.User;
-import org.prgrms.nabimarketbe.domain.user.repository.UserJpaRepo;
+import org.prgrms.nabimarketbe.domain.user.repository.UserRepository;
+import org.prgrms.nabimarketbe.global.security.entity.RefreshToken;
+import org.prgrms.nabimarketbe.global.security.jwt.dto.TokenRequestDto;
+import org.prgrms.nabimarketbe.global.security.jwt.dto.TokenResponseDTO;
+import org.prgrms.nabimarketbe.global.security.jwt.provider.JwtProvider;
 import org.prgrms.nabimarketbe.global.util.ResponseFactory;
 import org.prgrms.nabimarketbe.global.util.model.CommonResult;
+import org.prgrms.nabimarketbe.oauth2.google.dto.GoogleUserInfoDTO;
+import org.prgrms.nabimarketbe.oauth2.kakao.dto.KakaoProfile;
+import org.prgrms.nabimarketbe.oauth2.kakao.repository.RefreshTokenJpaRepo;
+import org.prgrms.nabimarketbe.oauth2.kakao.service.OAuth2Service;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class SignService {
-    private final UserJpaRepo userJpaRepo;
+    private final UserRepository userJpaRepo;
 
-    private final OAuth2Service OAuth2Service;
+    private final OAuth2Service oAuth2Service;
 
     private final ResponseFactory responseFactory;
 
@@ -36,7 +38,7 @@ public class SignService {
 
     @Transactional
     public CommonResult signupBySocial(String accessToken) {
-        KakaoProfile kakaoProfile = OAuth2Service.getKakaoProfile(accessToken);
+        KakaoProfile kakaoProfile = oAuth2Service.getKakaoProfile(accessToken);
 
         if (kakaoProfile == null) throw new RuntimeException("카카오에 해당 회원이 없습니다.");
 
@@ -47,6 +49,7 @@ public class SignService {
 
         return responseFactory.getSingleResult(result);
     }
+
     @Transactional
     public CommonResult socialSignup(UserSignupRequestDto userSignupRequestDto) {
         Optional<User> user = userJpaRepo.findByNicknameAndProvider(
@@ -63,7 +66,7 @@ public class SignService {
     }
 
     @Transactional
-    public TokenDto reissue(TokenRequestDto tokenRequestDto) {
+    public TokenResponseDTO reissue(TokenRequestDto tokenRequestDto) {
         // 만료된 refresh token 에러
         if (!jwtProvider.validationToken(tokenRequestDto.getAccessToken())) {
             throw new RuntimeException("RefreshTokenException");
@@ -77,7 +80,7 @@ public class SignService {
         User user = userJpaRepo.findById(Long.parseLong(authentication.getName()))
                 .orElseThrow(() ->new RuntimeException("RefreshTokenException"));
 
-        RefreshToken refreshToken = tokenJpaRepo.findByKey(user.getUserId())
+        RefreshToken refreshToken = tokenJpaRepo.findByUserId(user.getUserId())
                 .orElseThrow(() ->new RuntimeException("RefreshTokenException"));
 
         // 리프레시 토큰 불일치 에러
@@ -85,11 +88,22 @@ public class SignService {
             throw new RuntimeException("RefreshTokenException");
 
         // AccessToken, RefreshToken 토큰 재발급, 리프레쉬 토큰 저장
-        TokenDto newCreatedToken = jwtProvider.createTokenDto(user.getUserId(), user.getRoles());
+        TokenResponseDTO newCreatedToken = jwtProvider.createTokenDto(user.getUserId(), user.getRoles());
         RefreshToken updateRefreshToken = refreshToken.updateToken(newCreatedToken.getRefreshToken());
 
         tokenJpaRepo.save(updateRefreshToken);
 
         return newCreatedToken;
+    }
+
+    @Transactional
+    public User signUp(GoogleUserInfoDTO googleUserInfoDTO) {
+        User user = googleUserInfoDTO.toEntity("randomNickName");
+        User savedUser = userJpaRepo.save(user);
+
+        // TODO : 닉네임 랜덤 생성
+        log.info("modified date : {}", savedUser.getModifiedDate());
+
+        return savedUser;
     }
 }
--- src/main/java/org/prgrms/nabimarketbe/oauth2/google/service/GoogleOAuth2Service.java
+++ src/main/java/org/prgrms/nabimarketbe/oauth2/google/service/GoogleOAuth2Service.java
@@ -3,34 +3,33 @@ package org.prgrms.nabimarketbe.oauth2.google.service;
 import java.util.Optional;
 
 import org.prgrms.nabimarketbe.domain.user.dto.UserLoginResponseDTO;
-import org.prgrms.nabimarketbe.domain.user.dto.UserResponseDto;
 import org.prgrms.nabimarketbe.domain.user.entity.User;
 import org.prgrms.nabimarketbe.domain.user.repository.UserRepository;
-import org.prgrms.nabimarketbe.global.security.entity.RefreshToken;
+import org.prgrms.nabimarketbe.domain.user.service.SignService;
 import org.prgrms.nabimarketbe.global.security.jwt.dto.TokenResponseDTO;
 import org.prgrms.nabimarketbe.global.security.jwt.provider.JwtProvider;
 import org.prgrms.nabimarketbe.oauth2.google.domain.OAuth2;
-import org.prgrms.nabimarketbe.oauth2.google.dto.GoogleOAuth2Token;
-import org.prgrms.nabimarketbe.oauth2.google.dto.GoogleUser;
-import org.prgrms.nabimarketbe.oauth2.kakao.repository.RefreshTokenJpaRepo;
+import org.prgrms.nabimarketbe.oauth2.google.dto.GoogleOAuth2TokenDTO;
+import org.prgrms.nabimarketbe.oauth2.google.dto.GoogleUserInfoDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class GoogleOAuth2Service {
-
 	private final OAuth2 oAuth2;
 
 	private final JwtProvider jwtProvider;
 
 	private final UserRepository userRepository;
 
-	private final RefreshTokenJpaRepo tokenJpaRepo;
+	private final SignService signService;
 
 	public String requestRedirectUrl() {
 		String redirectUrl = oAuth2.getOAuth2RedirectUrl();
@@ -39,41 +38,25 @@ public class GoogleOAuth2Service {
 	}
 
 	public UserLoginResponseDTO OAuth2Login(String code) throws JsonProcessingException {
-		ResponseEntity<String> accessToken = oAuth2.requestAccessToken(code);
-		GoogleOAuth2Token googleOAuth2Token = oAuth2.getAccessToken(accessToken);
+		GoogleUserInfoDTO googleUserInfoDTO = getUserInfoFromPlatform(code);
+		String nameAttributeKey = googleUserInfoDTO.id();
 
-		ResponseEntity<String> userInfo = oAuth2.requestUserInfo(googleOAuth2Token);
+		Optional<User> optionalUser = userRepository.findByNameAttributeKey(nameAttributeKey);
 
-		GoogleUser googleUser = oAuth2.parseUserInfo(userInfo);
+		User user = optionalUser.orElseGet(() -> signService.signUp(googleUserInfoDTO));
+		TokenResponseDTO tokenResponseDTO = jwtProvider.createTokenDto(user.getUserId(), user.getRoles());
 
-		String nameAttributeKey = googleUser.id();
+		UserLoginResponseDTO response = UserLoginResponseDTO.from(user, tokenResponseDTO);
 
-		Optional<User> optionalUser = userRepository.findByNameAttributeKey(nameAttributeKey);ㅋ
-		if(optionalUser.isPresent()) {
-			User user = optionalUser.get();
-			UserResponseDto userResponseDto = UserResponseDto.from(user);
-			TokenResponseDTO token = jwtProvider.createTokenDto(user.getUserId(), user.getRoles());
-
-			UserLoginResponseDTO response = new UserLoginResponseDTO(userResponseDto, token);
-			return response;
-		}
-
-		UserResponseDto userResponseDto = signUpUser(googleUser);
-		TokenResponseDTO token = jwtProvider.createTokenDto(userResponseDto.getUserId(), userResponseDto.getRoles());
-
-		RefreshToken refreshToken = new RefreshToken(userResponseDto.getUserId(), token.getRefreshToken());
-		tokenJpaRepo.save(refreshToken);
-
-		UserLoginResponseDTO response = new UserLoginResponseDTO(userResponseDto, token);
 		return response;
 	}
 
-	private UserResponseDto signUpUser(GoogleUser googleUser) {
-		// TODO : 닉네임 자동생성
-		User user = googleUser.toEntity("tmpNickName");
-		User savedUser = userRepository.save(user);
+	private GoogleUserInfoDTO getUserInfoFromPlatform(String code) throws JsonProcessingException {
+		ResponseEntity<String> accessToken = oAuth2.requestAccessToken(code);
+		GoogleOAuth2TokenDTO googleOAuth2TokenDTO = oAuth2.getAccessToken(accessToken);
 
-		return UserResponseDto.from(savedUser);
+		ResponseEntity<String> userInfo = oAuth2.requestUserInfo(googleOAuth2TokenDTO);
+
+		return oAuth2.parseUserInfo(userInfo);
 	}
-
 }
