b9966b871bd46e418e2a5c5c521f057592e61c2d
hi-june <wnsgur735@gmail.com>
2023-11-02 18:10:19 +0900

    feat: category 도메인 bean validation 추가

--- src/main/java/org/prgrms/nabimarketbe/domain/category/entity/Category.java
+++ src/main/java/org/prgrms/nabimarketbe/domain/category/entity/Category.java
@@ -2,6 +2,7 @@ package org.prgrms.nabimarketbe.domain.category.entity;
 
 import lombok.Getter;
 import org.prgrms.nabimarketbe.global.BaseEntity;
+import org.prgrms.nabimarketbe.global.annotation.ValidEnum;
 
 import javax.persistence.*;
 
@@ -11,8 +12,11 @@ import javax.persistence.*;
 public class Category extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id", nullable = false)
     private Long categoryId;
 
+    @ValidEnum(enumClass = CategoryEnum.class, message = "유효하지 않은 카테고리입니다.")
     @Enumerated(EnumType.STRING)
+    @Column(name = "category_name", nullable = false)
     private CategoryEnum categoryName;
 }
