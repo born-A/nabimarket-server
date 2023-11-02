4d934d9485bac3e327ed54237ab7293a0365f7de
hi-june <wnsgur735@gmail.com>
2023-11-02 18:09:36 +0900

    feat: card_image 도메인 bean validation 및 생성자 검증 추가

--- src/main/java/org/prgrms/nabimarketbe/domain/cardimage/dto/request/CardImageCreateRequestDTO.java
+++ src/main/java/org/prgrms/nabimarketbe/domain/cardimage/dto/request/CardImageCreateRequestDTO.java
@@ -1,6 +1,5 @@
 package org.prgrms.nabimarketbe.domain.cardimage.dto.request;
 
-import org.prgrms.nabimarketbe.domain.cardimage.entity.CardImage;
 
 public record CardImageCreateRequestDTO(
         Integer _id,
--- src/main/java/org/prgrms/nabimarketbe/domain/cardimage/entity/CardImage.java
+++ src/main/java/org/prgrms/nabimarketbe/domain/cardimage/entity/CardImage.java
@@ -1,22 +1,29 @@
 package org.prgrms.nabimarketbe.domain.cardimage.entity;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.prgrms.nabimarketbe.global.BaseEntity;
 import org.prgrms.nabimarketbe.domain.card.entity.Card;
+import org.prgrms.nabimarketbe.global.error.BaseException;
+import org.prgrms.nabimarketbe.global.error.ErrorCode;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "card_images")
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class CardImage extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "image_id", nullable = false)
     private Long cardImageId;
 
+    @NotBlank(message = "공백을 허용하지 않습니다.")
+    @Column(name = "image_url", nullable = false)
     private String imageUrl;
 
     @ManyToOne
@@ -28,6 +35,14 @@ public class CardImage extends BaseEntity {
             String imageUrl,
             Card card
     ) {
+        if (imageUrl.isBlank()) {
+            throw new BaseException(ErrorCode.UNKNOWN);
+        }
+
+        if (card == null) {
+            throw new BaseException(ErrorCode.UNKNOWN);
+        }
+
         this.imageUrl = imageUrl;
         this.card = card;
     }
