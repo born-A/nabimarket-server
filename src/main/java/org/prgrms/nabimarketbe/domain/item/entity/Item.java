da5d75ff01c140d669869695c28b725ea9e4fb15
hi-june <wnsgur735@gmail.com>
2023-11-02 18:09:57 +0900

    feat: item 도메인 bean validation 및 생성자 검증 추가

--- src/main/java/org/prgrms/nabimarketbe/domain/item/entity/Item.java
+++ src/main/java/org/prgrms/nabimarketbe/domain/item/entity/Item.java
@@ -1,25 +1,35 @@
 package org.prgrms.nabimarketbe.domain.item.entity;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.prgrms.nabimarketbe.global.BaseEntity;
 import org.prgrms.nabimarketbe.domain.category.entity.Category;
+import org.prgrms.nabimarketbe.global.annotation.ValidEnum;
+import org.prgrms.nabimarketbe.global.error.BaseException;
+import org.prgrms.nabimarketbe.global.error.ErrorCode;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "items")
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class Item extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_id", nullable = false)
     private Long itemId;
 
+    @NotBlank(message = "공백을 허용하지 않습니다.")
+    @Column(name = "item_name", nullable = false)
     private String itemName;
 
+    @ValidEnum(enumClass = PriceRange.class, message = "유효하지 않은 가격대입니다.")
     @Enumerated(EnumType.STRING)
+    @Column(name = "price_range", nullable = false)
     private PriceRange priceRange;
 
     @OneToOne
@@ -32,6 +42,14 @@ public class Item extends BaseEntity {
             PriceRange priceRange,
             Category category
     ) {
+        if (itemName.isBlank()) {
+            throw new BaseException(ErrorCode.UNKNOWN);
+        }
+
+        if (priceRange == null || category == null) {
+            throw new BaseException(ErrorCode.UNKNOWN);
+        }
+
         this.itemName = itemName;
         this.priceRange = priceRange;
         this.category = category;
