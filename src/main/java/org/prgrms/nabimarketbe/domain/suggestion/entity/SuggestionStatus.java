77d9b2b8fa66d08d3e86e2a9df26ad08dc1f8e6c
born-A <93516595+born-A@users.noreply.github.com>
2023-11-08 02:27:16 +0900

    NABI-125--feat : suggestion 타입, 상태 enum

---
+++ src/main/java/org/prgrms/nabimarketbe/domain/suggestion/entity/SuggestionStatus.java
@@ -0,0 +1,7 @@
+package org.prgrms.nabimarketbe.domain.suggestion.entity;
+
+public enum SuggestionStatus {
+    WAITING,
+    ACCEPTED,
+    REFUSED
+}
---
+++ src/main/java/org/prgrms/nabimarketbe/domain/suggestion/entity/SuggestionType.java
@@ -0,0 +1,6 @@
+package org.prgrms.nabimarketbe.domain.suggestion.entity;
+
+public enum SuggestionType {
+    OFFER,
+    POKE
+}
