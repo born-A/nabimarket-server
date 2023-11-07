48aff6a1517bbe476477613cf5f9f4234658c6f3
BeommoKoo-dev <95630007+BeommoKoo-dev@users.noreply.github.com>
2023-11-07 22:13:51 +0900

    NO-JIRA--chore : CICD workflow 업데이트

--- src/main/java/org/prgrms/nabimarketbe/global/security/jwt/dto/TokenResponseDTO.java
+++ src/main/java/org/prgrms/nabimarketbe/global/security/jwt/dto/TokenResponseDTO.java
@@ -2,17 +2,11 @@ package org.prgrms.nabimarketbe.global.security.jwt.dto;
 
 import lombok.*;
 
-@Getter
-@Setter
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class TokenResponseDTO {
-    private String grantType;
-
-    private String accessToken;
-
-    private String refreshToken;
-
-    private Long accessTokenExpireDate;
+public record TokenResponseDTO(
+    String grantType,
+    String accessToken,
+    String refreshToken,
+    Long accessTokenExpireDate
+) {
 }
