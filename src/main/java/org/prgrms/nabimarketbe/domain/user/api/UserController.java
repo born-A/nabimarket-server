7759b9a0bb9d3f8941769f8f4ee75f24e20084d0
BeommoKoo-dev <95630007+BeommoKoo-dev@users.noreply.github.com>
2023-11-08 20:23:35 +0900

    NO-JIRA--refactor : user응답포맷 획일화

--- src/main/java/org/prgrms/nabimarketbe/domain/user/api/UserController.java
+++ src/main/java/org/prgrms/nabimarketbe/domain/user/api/UserController.java
@@ -7,6 +7,7 @@ import org.prgrms.nabimarketbe.domain.user.dto.response.UserUpdateResponseDTO;
 import org.prgrms.nabimarketbe.domain.user.service.UserService;
 import org.prgrms.nabimarketbe.global.util.ResponseFactory;
 import org.prgrms.nabimarketbe.global.util.model.SingleResult;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,32 +26,29 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping
-    public SingleResult<UserResponseDTO> getUserByToken(@RequestHeader(name = "authorization") String token) {
-        UserGetResponseDTO userGetResponseDTO = userService.getUserByToken(token);
-        UserResponseDTO userResponseDTO = new UserResponseDTO(userGetResponseDTO);
+    public ResponseEntity<SingleResult<UserResponseDTO<UserGetResponseDTO>>> getUserByToken(@RequestHeader(name = "authorization") String token) {
+        UserResponseDTO<UserGetResponseDTO> userResponseDTO = userService.getUserByToken(token);
 
-        return ResponseFactory.getSingleResult(userResponseDTO);
+        return ResponseEntity.ok(ResponseFactory.getSingleResult(userResponseDTO));
     }
 
     @PutMapping("/profile-image")
-    public SingleResult<UserResponseDTO> updateUserImageUrl(
+    public ResponseEntity<SingleResult<UserResponseDTO<UserUpdateResponseDTO>>> updateUserImageUrl(
         @RequestHeader(name = "authorization") String token,
         @RequestPart("file") MultipartFile file
     ) {
-        UserUpdateResponseDTO userUpdateResponseDTO = userService.updateUserImageUrl(token, file);
-        UserResponseDTO userResponseDTO = new UserResponseDTO(userUpdateResponseDTO);
+        UserResponseDTO<UserUpdateResponseDTO> userResponseDTO = userService.updateUserImageUrl(token, file);
 
-        return ResponseFactory.getSingleResult(userResponseDTO);
+        return ResponseEntity.ok(ResponseFactory.getSingleResult(userResponseDTO));
     }
 
     @PutMapping("/nickname")
-    public SingleResult<UserResponseDTO> updateUserNickname(
+    public ResponseEntity<SingleResult<UserResponseDTO<UserUpdateResponseDTO>>> updateUserNickname(
         @RequestHeader(name = "authorization") String token,
         @RequestBody UserUpdateRequestDTO userUpdateRequestDTO
     ) {
-        UserUpdateResponseDTO userUpdateResponseDTO = userService.updateUserNickname(token, userUpdateRequestDTO);
-        UserResponseDTO userResponseDTO = new UserResponseDTO(userUpdateResponseDTO);
+        UserResponseDTO<UserUpdateResponseDTO> userResponseDTO = userService.updateUserNickname(token, userUpdateRequestDTO);
 
-        return ResponseFactory.getSingleResult(userResponseDTO);
+        return ResponseEntity.ok(ResponseFactory.getSingleResult(userResponseDTO));
     }
 }
