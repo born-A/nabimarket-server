d85b6177debaf970aa0ab1cc1f769d9a9fb85553
born-A <93516595+born-A@users.noreply.github.com>
2023-11-09 14:27:53 +0900

    NABI-125--feat : checkToken 오버로딩

---
+++ src/main/java/org/prgrms/nabimarketbe/domain/user/service/CheckService.java
@@ -0,0 +1,41 @@
+package org.prgrms.nabimarketbe.domain.user.service;
+
+import java.util.Objects;
+
+import org.prgrms.nabimarketbe.domain.card.entity.Card;
+import org.prgrms.nabimarketbe.global.error.BaseException;
+import org.prgrms.nabimarketbe.global.error.ErrorCode;
+import org.prgrms.nabimarketbe.global.security.jwt.provider.JwtProvider;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CheckService {
+    private final JwtProvider jwtProvider;
+
+    public Long parseToken(String token) {
+        Claims claims = jwtProvider.parseClaims(token);
+        Long userId = Long.valueOf(claims.getSubject());
+
+        return userId;
+    }
+
+    public void checkToken(String token, Long userId) {
+        Long tokenUserId = parseToken(token);
+
+        if(!Objects.equals(tokenUserId, userId)) {
+            throw new BaseException(ErrorCode.USER_NOT_MATCHED);
+        }
+    }
+
+    public void checkToken(String token, Card card) {
+        Long tokenUserId = parseToken(token);
+
+        if(!Objects.equals(tokenUserId, card.getUser().getUserId())) {
+            throw new BaseException(ErrorCode.USER_NOT_MATCHED);
+        }
+    }
+}
