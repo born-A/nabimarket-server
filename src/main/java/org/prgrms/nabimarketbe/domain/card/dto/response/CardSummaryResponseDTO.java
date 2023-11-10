b7ec1a0d7b5a71498ff604558fdc121e8b5757b0
born-A <93516595+born-A@users.noreply.github.com>
2023-11-10 18:53:32 +0900

    NABI-184--refactor : 생성자 삭제

--- src/main/java/org/prgrms/nabimarketbe/domain/card/dto/response/CardSummaryResponseDTO.java
+++ src/main/java/org/prgrms/nabimarketbe/domain/card/dto/response/CardSummaryResponseDTO.java
@@ -1,13 +1,10 @@
 package org.prgrms.nabimarketbe.domain.card.dto.response;
 
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import org.prgrms.nabimarketbe.domain.item.entity.PriceRange;
 
 @Getter
-@NoArgsConstructor
 public class CardSummaryResponseDTO{
     private Long cardId;
 
@@ -16,17 +13,4 @@ public class CardSummaryResponseDTO{
     private String thumbnail;
 
     private PriceRange priceRange;
-
-    @Builder
-    private CardSummaryResponseDTO(
-        Long cardId,
-        String itemName,
-        String thumbnail,
-        PriceRange priceRange
-    ){
-        this.cardId = cardId;
-        this.itemName = itemName;
-        this.thumbnail = thumbnail;
-        this.priceRange = priceRange;
-    }
 }
--- src/main/java/org/prgrms/nabimarketbe/domain/completeRequest/dto/response/HistoryListReadResponseDTO.java
+++ src/main/java/org/prgrms/nabimarketbe/domain/completeRequest/dto/response/HistoryListReadResponseDTO.java
@@ -1,12 +1,11 @@
 package org.prgrms.nabimarketbe.domain.completeRequest.dto.response;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+
 import org.prgrms.nabimarketbe.domain.card.dto.response.CardSummaryResponseDTO;
 
 import java.time.LocalDateTime;
 
-@NoArgsConstructor
 @Getter
 public class HistoryListReadResponseDTO {
     private CardSummaryResponseDTO fromCard;
