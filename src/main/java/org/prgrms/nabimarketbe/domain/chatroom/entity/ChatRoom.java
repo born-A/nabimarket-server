8066a0c0850f4bd2f1ed4b3bf800a772f2bd38f2
hi-june <wnsgur735@gmail.com>
2023-11-22 01:41:25 +0900

    NABI-263--feat : 채팅방 엔티티 정의

---
+++ src/main/java/org/prgrms/nabimarketbe/domain/chatroom/entity/ChatRoom.java
@@ -0,0 +1,44 @@
+package org.prgrms.nabimarketbe.domain.chatroom.entity;
+
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.prgrms.nabimarketbe.domain.suggestion.entity.Suggestion;
+import org.prgrms.nabimarketbe.global.BaseEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "chat_rooms")
+@NoArgsConstructor
+public class ChatRoom extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "chat_room_id")
+    private Long chatRoomId;
+
+    @NotBlank
+    @Column(nullable = false, name = "firestore_chat_room_path")
+    private String fireStoreChatRoomPath;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, name = "suggestion_id")
+    private Suggestion suggestion;
+
+    @Builder
+    public ChatRoom(
+            String fireStoreChatRoomPath,
+            Suggestion suggestion
+    ) {
+        this.fireStoreChatRoomPath = fireStoreChatRoomPath;
+        this.suggestion = suggestion;
+    }
+}
---
+++ src/main/java/org/prgrms/nabimarketbe/domain/chatroom/repository/ChatRoomRepository.java
@@ -0,0 +1,7 @@
+package org.prgrms.nabimarketbe.domain.chatroom.repository;
+
+import org.prgrms.nabimarketbe.domain.chatroom.entity.ChatRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+}
