199011ee93523719c5abe3944e7c82afc0768ab8
BeommoKoo-dev <95630007+BeommoKoo-dev@users.noreply.github.com>
2023-11-23 22:55:11 +0900

    NABI-298--rename : test fixture 메소드명 변경

--- src/test/java/org/prgrms/nabimarketbe/setup/user/UserBuilder.java
+++ src/test/java/org/prgrms/nabimarketbe/setup/user/UserBuilder.java
@@ -4,7 +4,7 @@ import org.prgrms.nabimarketbe.domain.user.Role;
 import org.prgrms.nabimarketbe.domain.user.entity.User;
 
 public class UserBuilder {
-    public static User build() {
+    public static User createTestUser() {
         return User.builder()
             .accountId("testAccountId")
             .nickname("testNickname")
