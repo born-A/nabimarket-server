cbc02ae475cc7aafe8f89ed2618be32ece010209
BeommoKoo-dev <95630007+BeommoKoo-dev@users.noreply.github.com>
2023-11-23 16:43:33 +0900

    NABI-298--refactor : 카카오, 구글 로그인dto 통합

--- src/main/java/org/prgrms/nabimarketbe/domain/user/service/SignService.java
+++ src/main/java/org/prgrms/nabimarketbe/domain/user/service/SignService.java
@@ -2,7 +2,7 @@ package org.prgrms.nabimarketbe.domain.user.service;
 
 import java.util.Optional;
 
-import org.prgrms.nabimarketbe.domain.user.dto.request.UserSignInRequestDTO;
+import org.prgrms.nabimarketbe.domain.user.dto.request.SocialUserInfoDTO;
 import org.prgrms.nabimarketbe.domain.user.dto.response.UserLoginResponseDTO;
 import org.prgrms.nabimarketbe.domain.user.entity.User;
 import org.prgrms.nabimarketbe.domain.user.repository.UserRepository;
@@ -10,7 +10,6 @@ import org.prgrms.nabimarketbe.global.error.BaseException;
 import org.prgrms.nabimarketbe.global.error.ErrorCode;
 import org.prgrms.nabimarketbe.global.security.jwt.dto.TokenResponseDTO;
 import org.prgrms.nabimarketbe.global.security.jwt.provider.JwtProvider;
-import org.prgrms.nabimarketbe.oauth2.google.dto.GoogleUserInfoDTO;
 import org.prgrms.nabimarketbe.oauth2.kakao.dto.KakaoProfile;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,31 +26,30 @@ public class SignService {
     private final UserRepository userRepository;
 
     private final JwtProvider jwtProvider;
-    
+
     private final RandomNicknameGenerator randomNicknameGenerator;
 
     @Transactional
     public UserLoginResponseDTO signInBySocial(KakaoProfile kakaoProfile) {
-        UserSignInRequestDTO userSignInRequestDTO = UserSignInRequestDTO.builder()
-                .accountId(kakaoProfile.getId())
-                .nickname(kakaoProfile.getProperties().getNickname())
-                .provider("kakao")
-                .build();
+        SocialUserInfoDTO socialUserInfoDTO = SocialUserInfoDTO.builder()
+            .accountId(kakaoProfile.getId())
+            .provider("KAKAO")
+            .build();
 
-        UserLoginResponseDTO result = signIn(userSignInRequestDTO);
+        UserLoginResponseDTO result = signIn(socialUserInfoDTO);
 
         return result;
     }
 
     @Transactional
-    public UserLoginResponseDTO signIn(UserSignInRequestDTO userSignInRequestDTO) {
-        String accountId = userSignInRequestDTO.accountId();
+    public UserLoginResponseDTO signIn(SocialUserInfoDTO socialUserInfoDTO) {
+        String accountId = socialUserInfoDTO.accountId();
 
         Optional<User> optionalUser = userRepository.findByAccountId(accountId);
 
         User user = optionalUser.orElseGet(() -> {
             try {
-                return signUp(userSignInRequestDTO);
+                return signUp(socialUserInfoDTO);
             } catch (JsonProcessingException e) {
                 throw new BaseException(ErrorCode.EXTERNAL_SERVER_ERROR);
             }
@@ -65,38 +63,9 @@ public class SignService {
     }
 
     @Transactional
-    public UserLoginResponseDTO signIn(GoogleUserInfoDTO userInfo) {
-        String accountId = userInfo.id();
-
-        Optional<User> optionalUser = userRepository.findByAccountId(accountId);
-
-        User user = optionalUser.orElseGet(() -> {
-            try {
-                return signUp(userInfo);
-            } catch (JsonProcessingException e) {
-                throw new BaseException(ErrorCode.EXTERNAL_SERVER_ERROR);
-            }
-        });
-        TokenResponseDTO tokenResponseDTO = jwtProvider.createTokenDTO(user.getUserId(), user.getRole());
-
-        UserLoginResponseDTO response = UserLoginResponseDTO.of(user, tokenResponseDTO);
-
-        return response;
-    }
-
-    @Transactional
-    public User signUp(GoogleUserInfoDTO googleUserInfoDTO) throws JsonProcessingException {
+    public User signUp(SocialUserInfoDTO socialUserInfoDTO) throws JsonProcessingException {
         String randomNickname = randomNicknameGenerator.generateRandomNickname();
-        User user = googleUserInfoDTO.toEntity(randomNickname);
-        User savedUser = userRepository.save(user);
-
-        return savedUser;
-    }
-
-    @Transactional
-    public User signUp(UserSignInRequestDTO userSignInRequestDTO) throws JsonProcessingException {
-        String randomNickname = randomNicknameGenerator.generateRandomNickname();
-        User user = userSignInRequestDTO.toEntity(randomNickname);
+        User user = socialUserInfoDTO.toEntity(randomNickname);
         User savedUser = userRepository.save(user);
 
         return savedUser;
