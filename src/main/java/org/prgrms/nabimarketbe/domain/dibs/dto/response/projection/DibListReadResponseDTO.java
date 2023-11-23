51824b7abeb0c2c61fbcdde26116dfd001519f70
BeommoKoo-dev <95630007+BeommoKoo-dev@users.noreply.github.com>
2023-11-23 16:49:17 +0900

    NO-JIRA--hotfix : api명세에 맞게 코드 수정

--- src/main/java/org/prgrms/nabimarketbe/domain/dibs/dto/response/projection/DibListReadResponseDTO.java
+++ src/main/java/org/prgrms/nabimarketbe/domain/dibs/dto/response/projection/DibListReadResponseDTO.java
@@ -1,25 +1,29 @@
 package org.prgrms.nabimarketbe.domain.dibs.dto.response.projection;
 
+import java.time.LocalDateTime;
+
 import org.prgrms.nabimarketbe.domain.card.entity.CardStatus;
 import org.prgrms.nabimarketbe.domain.item.entity.PriceRange;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
 @Getter
 public class DibListReadResponseDTO {
-	private Long dibId;
+    private Long dibId;
 
-	private Long cardId;
+    private Long cardId;
 
-	private String cardTitle;
+    private String cardTitle;
 
-	private String itemName;
+    private String itemName;
 
-	private PriceRange priceRange;
+    private PriceRange priceRange;
 
-	private String thumbNail;
+    private String thumbnail;
 
-	private CardStatus status;
+    private CardStatus status;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime modifiedAt;
 }
