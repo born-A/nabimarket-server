df13bf81496ce62125bb476c3099f3c4706648ae
born-A <93516595+born-A@users.noreply.github.com>
2023-11-24 16:29:19 +0900

    NABI-306--refactor : Suggestion A->B / B->A 막기

--- src/main/java/org/prgrms/nabimarketbe/domain/suggestion/repository/SuggestionRepository.java
+++ src/main/java/org/prgrms/nabimarketbe/domain/suggestion/repository/SuggestionRepository.java
@@ -6,11 +6,22 @@ import org.prgrms.nabimarketbe.domain.card.entity.Card;
 import org.prgrms.nabimarketbe.domain.suggestion.entity.Suggestion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SuggestionRepository extends JpaRepository<Suggestion, Long>, SuggestionRepositoryCustom {
     @Query("select s "
         + "from Suggestion s "
         + "where s.fromCard = :fromCard and s.toCard = :toCard "
         + "or s.fromCard = :toCard and s.toCard = :fromCard")
-    Optional<Suggestion> findSuggestionByFromCardAndToCard(Card fromCard, Card toCard);
+    Optional<Suggestion> findSuggestionByFromCardAndToCard(
+        @Param("fromCard") Card fromCard,
+        @Param("toCard") Card toCard
+    );
+
+    @Query(
+        "select count(s.suggestionId) > 0 " +
+            "from Suggestion s "
+            + "where s.fromCard = :fromCard and s.toCard = :toCard "
+        + "or s.fromCard = :toCard and s.toCard = :fromCard")
+    Boolean exists(@Param("fromCard") Card fromCard, @Param("toCard") Card toCard);
 }
