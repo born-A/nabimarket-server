c115484a9f9bcf26af90a1553a152f6bfbc15c54
hi-june <wnsgur735@gmail.com>
2023-11-27 22:35:53 +0900

    NO-JIRA--hotfix : server time zone

--- src/main/java/org/prgrms/nabimarketbe/NabiMarketBeApplication.java
+++ src/main/java/org/prgrms/nabimarketbe/NabiMarketBeApplication.java
@@ -5,9 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class NabiMarketBeApplication {
+	@PostConstruct
+	public void setTimeZone(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication application = new SpringApplication(NabiMarketBeApplication.class);
