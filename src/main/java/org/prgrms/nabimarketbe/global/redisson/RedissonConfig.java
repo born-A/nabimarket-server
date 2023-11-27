252b986d94cc7bc7a29c79b062a736a59282cbcf
born-A <93516595+born-A@users.noreply.github.com>
2023-11-27 14:01:55 +0900

    NABI-198--refactor : RedissonConfig

--- src/main/java/org/prgrms/nabimarketbe/global/redisson/RedissonConfig.java
+++ src/main/java/org/prgrms/nabimarketbe/global/redisson/RedissonConfig.java
@@ -21,12 +21,12 @@ public class RedissonConfig {
     private int redisPort;
 
     private static final String REDISSON_HOST_PREFIX = "redis://";
+
     @Bean
     public RedissonClient redissonClient() {
-        RedissonClient redisson = null;
         Config config = new Config();
         config.useSingleServer().setAddress(REDISSON_HOST_PREFIX + redisHost + ":" + redisPort);
-        redisson = Redisson.create(config);
-        return redisson;
+
+        return Redisson.create(config);
     }
 }
