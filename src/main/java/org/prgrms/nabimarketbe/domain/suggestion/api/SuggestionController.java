77d5f85defb86a22a28432fb56e6c510b25d87b0
born-A <93516595+born-A@users.noreply.github.com>
2023-11-27 16:15:24 +0900

    NABI-198--feat : DistributedLock 적용 - create suggestion

--- src/main/java/org/prgrms/nabimarketbe/domain/suggestion/api/SuggestionController.java
+++ src/main/java/org/prgrms/nabimarketbe/domain/suggestion/api/SuggestionController.java
@@ -29,6 +29,7 @@ public class SuggestionController {
     ) {
         SuggestionResponseDTO suggestionResponseDTO = suggestionService.createSuggestion(
             token,
+            key,
             suggestionType,
             suggestionRequestDTO
         );
