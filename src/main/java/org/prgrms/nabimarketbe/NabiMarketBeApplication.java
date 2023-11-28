611e3df8f26766749da129466a993ff7300347b2
hi-june <wnsgur735@gmail.com>
2023-11-29 00:54:57 +0900

    NABI-351--chore : scheduling 설정 추가

--- src/main/java/org/prgrms/nabimarketbe/NabiMarketBeApplication.java
+++ src/main/java/org/prgrms/nabimarketbe/NabiMarketBeApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @EnableRetry
 @EnableJpaAuditing
+@EnableScheduling
 @SpringBootApplication
 public class NabiMarketBeApplication {
     @PostConstruct
