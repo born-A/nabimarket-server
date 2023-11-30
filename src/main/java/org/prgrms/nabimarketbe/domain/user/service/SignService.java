28bdb818b049f65e20105a0e11bd7ab2007e70fb
BeommoKoo-dev <95630007+BeommoKoo-dev@users.noreply.github.com>
2023-11-30 22:25:26 +0900

    NABI-372--feat : reissue 기능 구현

--- src/main/java/org/prgrms/nabimarketbe/domain/user/service/SignService.java
+++ src/main/java/org/prgrms/nabimarketbe/domain/user/service/SignService.java
@@ -8,8 +8,12 @@ import org.prgrms.nabimarketbe.domain.user.entity.User;
 import org.prgrms.nabimarketbe.domain.user.repository.UserRepository;
 import org.prgrms.nabimarketbe.global.error.BaseException;
 import org.prgrms.nabimarketbe.global.error.ErrorCode;
+import org.prgrms.nabimarketbe.global.security.entity.RefreshToken;
+import org.prgrms.nabimarketbe.global.security.jwt.dto.AccessTokenResponseDTO;
 import org.prgrms.nabimarketbe.global.security.jwt.dto.TokenResponseDTO;
 import org.prgrms.nabimarketbe.global.security.jwt.provider.JwtProvider;
+import org.prgrms.nabimarketbe.global.security.jwt.repository.RefreshTokenRepository;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,6 +31,8 @@ public class SignService {
     private final JwtProvider jwtProvider;
 
     private final RandomNicknameGenerator randomNicknameGenerator;
+
+    private final RefreshTokenRepository refreshTokenRepository;
 
     @Transactional
     public UserLoginResponseDTO signIn(SocialUserInfoDTO socialUserInfoDTO) {
@@ -47,6 +53,29 @@ public class SignService {
         UserLoginResponseDTO response = UserLoginResponseDTO.of(user, tokenResponseDTO);
 
         return response;
+    }
+
+    @Transactional
+    public AccessTokenResponseDTO reissue(String refreshToken) {
+        // 만료된 refresh token 에러
+        jwtProvider.validationToken(refreshToken);
+
+        Long userId = jwtProvider.parseUserId(refreshToken);
+
+        User user = userRepository.findById(userId)
+            .orElseThrow(() -> new UsernameNotFoundException("username is not found"));
+
+        RefreshToken refreshTokenEntity = refreshTokenRepository.findByUserId(userId)
+            .orElseThrow(() -> new BaseException(ErrorCode.USER_TOKEN_NOT_VALID));
+
+        if (!refreshTokenEntity.getToken().equals(refreshToken)) {
+            throw new BaseException(ErrorCode.USER_REFRESH_TOKEN_NOT_VALID);
+        }
+
+        AccessTokenResponseDTO accessTokenResponseDTO = jwtProvider.createAccessTokenDTO(user.getUserId(),
+            user.getRole());
+    
+        return accessTokenResponseDTO;
     }
 
     private User signUp(SocialUserInfoDTO socialUserInfoDTO) throws JsonProcessingException {
