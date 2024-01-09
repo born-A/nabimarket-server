98d3d5d678c43f0088e415b566c19d539ac239f8
hi-june <wnsgur735@gmail.com>
2024-01-09 15:23:53 +0900

    NABI-397--feat : user pojo 추가

---
+++ nabimarket-domain/src/main/java/org/prgrms/nabimarketbe/user/Role.java
@@ -0,0 +1,15 @@
+package org.prgrms.nabimarketbe.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+    GUEST("ROLE_GUEST"),
+
+    USER("ROLE_USER");
+
+    private final String key;
+}
+
---
+++ nabimarket-domain/src/main/java/org/prgrms/nabimarketbe/user/User.java
@@ -0,0 +1,50 @@
+package org.prgrms.nabimarketbe.user;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import org.prgrms.nabimarketbe.error.BaseException;
+import org.prgrms.nabimarketbe.error.ErrorCode;
+import org.prgrms.nabimarketbe.BasePOJO;
+
+@Getter
+public class User extends BasePOJO {
+    private Long userId;
+
+    private String accountId;
+
+    private String nickname;
+
+    private String imageUrl;
+
+    private String provider;
+
+    private String role;
+
+    @Builder
+    private User(
+        String accountId,
+        String nickname,
+        String imageUrl,
+        String provider,
+        String role
+    ) {
+        if (accountId.isBlank() || nickname.isBlank() || provider.isBlank() || role.isBlank()) {
+            throw new BaseException(ErrorCode.INVALID_REQUEST);
+        }
+
+        this.accountId = accountId;
+        this.nickname = nickname;
+        this.imageUrl = imageUrl;
+        this.provider = provider;
+        this.role = role;
+    }
+
+    public void updateNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void updateImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
