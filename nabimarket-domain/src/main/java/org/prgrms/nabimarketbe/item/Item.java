299d1f1a430b878b6283a83dcd618fbaca1d10ad
hi-june <wnsgur735@gmail.com>
2024-01-09 15:23:45 +0900

    NABI-397--feat : item pojo 추가

---
+++ nabimarket-domain/src/main/java/org/prgrms/nabimarketbe/item/Item.java
@@ -0,0 +1,49 @@
+package org.prgrms.nabimarketbe.item;
+
+import org.prgrms.nabimarketbe.BasePOJO;
+import org.prgrms.nabimarketbe.category.Category;
+
+import org.prgrms.nabimarketbe.error.ErrorCode;
+import org.prgrms.nabimarketbe.error.BaseException;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class Item extends BasePOJO {
+    private String itemName;
+
+    private PriceRange priceRange;
+
+    private Category category;
+
+    @Builder
+    private Item(
+        String itemName,
+        PriceRange priceRange,
+        Category category
+    ) {
+        if (itemName.isBlank()) {
+            throw new BaseException(ErrorCode.UNKNOWN);
+        }
+
+        if (priceRange == null || category == null) {
+            throw new BaseException(ErrorCode.UNKNOWN);
+        }
+
+        this.itemName = itemName;
+        this.priceRange = priceRange;
+        this.category = category;
+    }
+
+    public void updateItem(
+        String itemName,
+        PriceRange priceRange,
+        Category category
+    ) {
+        this.itemName = itemName;
+        this.priceRange = priceRange;
+        this.category = category;
+    }
+
+}
